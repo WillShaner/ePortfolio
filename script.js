fda7055cb43ca1a12cb467f0e0d31118f937525b
William Shaner <williamshaner3@gmail.com>
2023-05-09 21:37:39 -0500

    navbar and page setup

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const toggleNav = () => {
-  document.querySelector('.navbar-nav').classList.toggle('hidden')
-}
+// Toggle the dropdown menu on mobile screens
+const dropdownIcon = document.querySelector('.dropdown-icon');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+dropdownIcon.addEventListener('click', function() {
+  dropdownMenu.classList.toggle('show');
+  console.log('hit')
+});
